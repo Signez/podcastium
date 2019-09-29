16eb9c663b607400dabd651d19e648d72583080c
Stanislas Signoud (Signez) <signez@stanisoft.net>
2019-09-29 22:58:12 +0200

    Quickly add sample podcasts to browse

--- lib/ui/screens/add_podcast.dart
+++ lib/ui/screens/add_podcast.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:podcastium/ui/screens/podcast.dart';
 
-const String kTechTwoUrl =
-    'https://shows.blueprint.pm/tech-two/podcast_tech-two.xml';
+const String kProgrammeBUrl = '';
 
 class AddPodcastScreen extends StatelessWidget {
   @override
@@ -12,16 +11,62 @@ class AddPodcastScreen extends StatelessWidget {
         title: Text("Ajouter un podcast"),
       ),
       body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (BuildContext _context) =>
-                    PodcastScreen(feedUrl: kTechTwoUrl),
-              ),
-            );
-          },
-          child: Text("Ajouter Tech Two"),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            AddPodcastButton(
+              coverImageUrl: 'https://shows.blueprint.pm/tech-two/TechTwo.jpg',
+              podcastUrl:
+                  'https://shows.blueprint.pm/tech-two/podcast_tech-two.xml',
+              podcastTitle: 'Tech Two',
+            ),
+            AddPodcastButton(
+              coverImageUrl:
+                  'https://thumborcdn.acast.com/Xo8gkw7JAsa-dZhfKwUyy9bvE5A=/1500x1500/https://acast-media.s3.eu-west-1.amazonaws.com/assets/debd25e1-aef9-4791-b8eb-b4088df51216/1554710503262-319ca8683595c6c226b40af9019ace75.jpeg',
+              podcastUrl: 'https://rss.acast.com/programme-b',
+              podcastTitle: 'Programme B',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AddPodcastButton extends StatelessWidget {
+  final String podcastTitle;
+  final String coverImageUrl;
+  final String podcastUrl;
+
+  const AddPodcastButton({
+    this.podcastTitle,
+    this.podcastUrl,
+    this.coverImageUrl,
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: InkWell(
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (BuildContext _context) =>
+                  PodcastScreen(feedUrl: podcastUrl),
+            ),
+          );
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              PodcastCover(imageUrl: coverImageUrl),
+              const SizedBox(height: 16.0),
+              Text(podcastTitle),
+            ],
+          ),
         ),
       ),
     );
